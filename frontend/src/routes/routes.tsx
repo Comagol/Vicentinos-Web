3f937fba54a00539469dcffb9c936035d1c75f74
Comagol <comabellafrancisco@gmail.com>
2025-06-16 20:21:37 -0300

    adding the home view and creating the route / to home

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -3,10 +3,12 @@ import Login from '../views/Login'
 import PaymentStatus from '../views/PaymentStatus'
 import MemberCard from '../views/MemberCard'
 import News from '../views/News'
+import Home from '../views/Home'
 
 const AppRoutes = () => {
     return (
     <Routes>
+        <Route path="/" element={<Home />} />   
         <Route path="/login" element={<Login />} />
         <Route path="/member-card" element={<MemberCard />} />
         <Route path='/news' element={<News/>} />
