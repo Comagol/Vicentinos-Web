4efb2c18c4a0619eb4fad6bcedf91ba1b8a629d6
Comagol <comabellafrancisco@gmail.com>
2025-06-16 22:08:10 -0300

    adding the footer and the styles for it

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,23 +1,29 @@
-import { Link } from "react-router-dom"
-import Logo from '../assets/logo.jpg'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faInstagram } from "@fortawesome/free-brands-svg-icons"
-import './Footer.css'
-
+import { Link } from "react-router-dom";
+import Logo from '../assets/logo.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import './Footer.css';
 
 const Footer = () => {
   return (
     <div className="footerContainer">
-        <Link to="/">
-            <img src={Logo} alt="logo" />
-        </Link>
-        <div className="footerLinks">
-            <Link to="https://www.instagram.com/clubvicentinos/?hl=es" target="_blank">
-                <FontAwesomeIcon icon={faInstagram} />
-            </Link>
-        </div>
+      <Link to="/" className="footerLeft">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <div className="footerCenter">
+        <p>Copyright 2025 Club Vicentinos</p>
+        <p>Todos los derechos reservados</p>
+      </div>
+      <div className="footerLinks">
+        <a href="https://www.instagram.com/clubvicentinos/?hl=es" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="https://www.facebook.com/clubvicentinos/?locale=es_LA" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
