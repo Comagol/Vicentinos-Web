6ad5edb4300f66bf61b465eb94c6987f400c40f7
Comagol <comabellafrancisco@gmail.com>
2025-06-22 18:57:33 -0300

    adding the especified news services

---
+++ frontend/src/services/newsService.ts
@@ -0,0 +1,70 @@
+import api from "./api";
+
+//Tipos para las noticias
+export interface News {
+    id: number;
+    title: string;
+    description: string;
+    image: string;
+    date: string;
+    content: string;
+}
+
+//Servicio para obtener las noticias
+export const newsService = {
+    getAllNews: async (): Promise<News[]> => {
+        try {
+            const response = await api.get('/news');
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching news:', error);
+            throw error;
+        }
+    },
+
+    //obtener una noticia por su id
+    getNewById: async (id: number): Promise<News> => {
+        try {
+            const response = await api.get(`/news/${id}`);
+            return response.data;
+        } catch (error) {
+            console.error('error fetching news by id:', error);
+            throw error;
+        }
+    },
+
+    //Crear una nueva noticia
+    createNews: async (newsData: Omit<News, 'id'>): Promise<News> => {
+        try {
+            const response = await api.post('/news', newsData);
+            return response.data;
+        } catch (error) {
+            console.error('Error creating news', error);
+            throw error;
+        }
+    },
+
+    // Actualizar una noticia
+    update: async (id: number, newsData: Partial<News>): Promise<News> => {
+        try {
+        const response = await api.put(`/news/${id}`, newsData);
+        return response.data;
+        } catch (error) {
+        console.error(`Error updating news with id ${id}:`, error);
+        throw error;
+        }
+    },
+
+    //Eliminar una noticia
+    delete: async (id:number): Promise<void> => {
+        try {
+            await api.delete(`/news/${id}`);
+        } catch (error) {
+            console.error(`Error deleting news with id ${id}:`, error);
+            throw error;
+        }
+    },
+};
+
+
+export default newsService;
