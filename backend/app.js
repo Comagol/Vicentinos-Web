1a282abe897911247841df05b1abec451c88249a
Comagol <comabellafrancisco@gmail.com>
2025-06-30 19:20:20 -0300

    basic setup of app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
 import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+try {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+} catch (error) {
+    console.log(error)
+}
+
+export default app;
