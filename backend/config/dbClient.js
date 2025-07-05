acb17fd76f794a4f991a4bc043f2dcb272bd5d65
Comagol <comabellafrancisco@gmail.com>
2025-07-05 15:56:22 -0300

    adding db client config

---
+++ backend/config/dbClient.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI no esta definada en las variables de entorno");
+        }
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Conectado a MongoDB correctamente✅");
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
