b8fc92f9c484014ec6822e31672641fed9563daf
Comagol <comabellafrancisco@gmail.com>
2025-07-05 16:01:53 -0300

    adding the conection to the database config

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,25 @@
+// IMPORTACION DE LAS VARIABLES DE ENTORNO
+require("dotenv").config();
+
 // IMPORTO EL MODULO EXPRESS
-import express from "express";
+const express = require("express");
+
+//IMPORTACION DE HTTP
+const http = require("http");
+
+// IMPORTACION DE PATH
+const path = require("path");
+
+//importacion de la base de datos
+const connectDB = require("./config/dbClient");
 
 // CREO LA INSTANCIA DE EXPRESS
 const app = express();
+
+
+
+
+
 const PORT = process.env.PORT || 5000;
 
 try {
