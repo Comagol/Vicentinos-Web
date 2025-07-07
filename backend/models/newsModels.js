8d5386ef90c6d3b96fbd4c541ec4bcaad4cae6fb
Comagol <comabellafrancisco@gmail.com>
2025-07-06 21:34:26 -0300

    adding the newsmodels. js file

---
+++ backend/models/newsModels.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const newsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const NewsModel = mongoose.model("News", newsSchema);
+
+export default NewsModel;
