e568d0abbc0857aa33582a1f517372cafe2aaeb2
Comagol <comabellafrancisco@gmail.com>
2025-07-14 20:04:28 -0300

    adding the middleware to the routers

--- backend/routes/MemberRoutes.js
+++ backend/routes/MemberRoutes.js
@@ -1,20 +1,23 @@
 import express from "express";
 import MemberController from "../controllers/members.js";
+import authMiddleware from "../middlewares/auth.js";
 const router = express.Router();
 
+// Rutas Privadas (solo para el usuario autenticado)
+
 // Get = obtener todos los socios
-router.get("/", MemberController.getAllMembers);
+router.get("/", authMiddleware, MemberController.getAllMembers);
 
 // Get = obtener un socio por id
-router.get("/:id", MemberController.getMemberById);
+router.get("/:id", authMiddleware, MemberController.getMemberById);
 
 // Post = crear un nuevo socio
-router.post("/", MemberController.createMember); 
+router.post("/", authMiddleware, MemberController.createMember); 
 
 // Put = actualizar un socio
-router.put("/:id", MemberController.updateMember);
+router.put("/:id", authMiddleware, MemberController.updateMember);
 
 // Delete = eliminar un socio
-router.delete("/:id", MemberController.deleteMember);
+router.delete("/:id", authMiddleware, MemberController.deleteMember);
 
 export default router;
