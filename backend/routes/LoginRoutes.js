5f8751a7d9fd0c59c42f7939667e7a620650e52f
Comagol <comabellafrancisco@gmail.com>
2025-07-15 19:46:30 -0300

    adding the router

--- backend/routes/LoginRoutes.js
+++ backend/routes/LoginRoutes.js
@@ -3,5 +3,8 @@ import LoginController from "../controllers/login.js";
 import authMiddleware from "../middlewares/auth.js";
 import { setAuthCookie } from "../helpers/cookieHelper.js";
 
+//Creo el router
+const router = express.Router();
+
 //Ruta para el login
 router.post("/", LoginController.login);
