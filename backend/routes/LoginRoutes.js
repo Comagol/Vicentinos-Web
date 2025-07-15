8a5cbf5f1c0045676685131abe61534d3f7852cc
Comagol <comabellafrancisco@gmail.com>
2025-07-15 19:45:54 -0300

    starting to add routes to the login routes

--- backend/routes/LoginRoutes.js
+++ backend/routes/LoginRoutes.js
@@ -2,3 +2,6 @@ import express from "express";
 import LoginController from "../controllers/login.js";
 import authMiddleware from "../middlewares/auth.js";
 import { setAuthCookie } from "../helpers/cookieHelper.js";
+
+//Ruta para el login
+router.post("/", LoginController.login);
