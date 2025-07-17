f24542df29faf84e48363403f1a42dec51fdf3ae
Comagol <comabellafrancisco@gmail.com>
2025-07-17 19:43:19 -0300

    adding the route to the router

--- backend/routes/LoginRoutes.js
+++ backend/routes/LoginRoutes.js
@@ -8,4 +8,5 @@ const router = express.Router();
 
 //Ruta para el login
 router.post("/login", AuthController.login);
-router.post("/logout", AuthController.logout)
+router.post("/logout", AuthController.logout);
+router.post("/register", AuthController.register);
