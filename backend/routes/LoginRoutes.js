140d45be583bb25509a351a61acf6dfcd69da880
Comagol <comabellafrancisco@gmail.com>
2025-07-17 19:45:49 -0300

    fixing import problems

--- backend/routes/LoginRoutes.js
+++ backend/routes/LoginRoutes.js
@@ -1,7 +1,5 @@
 import express from "express";
 import AuthController from "../controllers/AuthController.js";
-import authMiddleware from "../middlewares/auth.js";
-import { setAuthCookie } from "../helpers/cookieHelper.js";
 
 //Creo el router
 const router = express.Router();
