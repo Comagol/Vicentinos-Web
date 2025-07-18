393c96fb935b5bc4a065c96a650b5c9aee16e69f
Comagol <comabellafrancisco@gmail.com>
2025-07-18 20:27:14 -0300

    adding a validation in the middleware auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,6 +4,9 @@ import jwt from "jsonwebtoken";
 const SECRET  = process.env.JWT_SECRET || "CLAVE_SECRETA";
 
 function authMiddleware(req, res, next) {
+  if (!req.cookie){
+    return res.status(401).json({ message: "No se encontraron las cookies en la petición"})
+  }
   const token = req.cookie.authToken;
   if (!token) {
     return res.status(401).json({ message: 'No se proporcionó un token de autenticación' });
