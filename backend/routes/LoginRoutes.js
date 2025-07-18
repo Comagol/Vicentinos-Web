077035efd0b5bc9bdd040d7b3428716017c86e21
Comagol <comabellafrancisco@gmail.com>
2025-07-18 20:06:58 -0300

    exporting router from loginRoutes

--- backend/routes/LoginRoutes.js
+++ backend/routes/LoginRoutes.js
@@ -8,3 +8,5 @@ const router = express.Router();
 router.post("/login", AuthController.login);
 router.post("/logout", AuthController.logout);
 router.post("/register", AuthController.register);
+
+export default router
