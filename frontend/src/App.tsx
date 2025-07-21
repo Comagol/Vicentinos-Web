d8076e1ce48af49b414a077499772b59b8732cef
Comagol <comabellafrancisco@gmail.com>
2025-07-21 19:52:29 -0300

    changing the app.tsx to show the footer at the end of the page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,18 @@ import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
 import AppRoutes from './routes/routes'
 import Footer from './components/Footer'
+import { Flex } from '@chakra-ui/react'
 
 function App() {
   return (
     <ThemeProvider>
+      <Flex direction="column" minH="100vh" flex="1">
       <Navbar />
+      <Flex as="main" flex="1" direction="column">
       <AppRoutes />
+      </Flex>
       <Footer />
+      </Flex>
     </ThemeProvider>
   )
 }
