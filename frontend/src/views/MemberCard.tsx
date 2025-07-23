f1a7d2e4f5eb6be19d5f91f199e15bfd42e6a277
Comagol <comabellafrancisco@gmail.com>
2025-07-22 23:40:55 -0300

    adding more staff to member card

--- frontend/src/views/MemberCard.tsx
+++ frontend/src/views/MemberCard.tsx
@@ -1,10 +1,12 @@
 import CardMember from "../components/CardMember";
-import { Flex, Text } from "@chakra-ui/react";
+import { Center, Flex, Text } from "@chakra-ui/react";
 
 const MemberCard = () => {
   return (
     <Flex alignSelf="center">
-      <Text>Carnet de Socio</Text>
+      <Center>
+      <Text fontSize="large">Carnet de Socio</Text>
+      </Center>
       <CardMember />
     </Flex>
   )
