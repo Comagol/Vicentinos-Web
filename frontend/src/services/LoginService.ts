cf710a771a6b8540cc7c3328acd89b6bae3771f2
Comagol <comabellafrancisco@gmail.com>
2025-07-25 20:15:06 -0300

    adding the loginservice to the front end, to conect the front with the back

---
+++ frontend/src/services/LoginService.ts
@@ -0,0 +1,31 @@
+import api from "./api";
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface RegisterData {
+  email: string;
+  password: string;
+  role?: string;
+}
+
+export const loginService = {
+  login: async (data: LoginData) => {
+    const response = await api.post("/login", data, { withCredentials: true});
+    return response.data;
+  },
+
+  logout: async () => {
+    const response = await api.post("/logout", {}, {withCredentials: true});
+    return response.data;
+  },
+
+  register: async (data: RegisterData) => {
+    const response = await api.post("/register", data, { withCredentials: true });
+    return response.data;
+  }
+};
+
+export default loginService;
